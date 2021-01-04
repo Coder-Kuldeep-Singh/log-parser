f4cc13c8a95a3aa4c7e31f9a63c631897363d1f5
mavensingh <kuldeep.avsar@gmail.com>
2021-01-04 20:28:23 +0530

    logs:added method to generate queue for next use

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"os"
 	"regexp"
+	"strings"
 )
 
 // error levels
@@ -48,35 +50,36 @@ func OpenFile(path string) (*os.File, error) {
 }
 
 // ReadFile reads the file
-func ReadFile(outcome *os.File) chan Logs {
-	rows := make(chan Logs)
-	go func() {
-		defer func() {
-			close(rows)
-		}()
-		scanner := bufio.NewScanner(outcome)
-		scanner.Split(bufio.ScanLines)
-
-		for scanner.Scan() {
-			matched := regularExpression(`^(\S+) (\S+) (\S+) \[([\w:/]+\s[+\-]\d{4})\] "(\S+) (\S+)\s*(\S*)" (\d{3}) (\S+) (\S+) "(.*)"`, scanner.Text())
-			if len(matched) == 0 {
-				continue
-			}
-			rows <- Logs{
-				ip:             matched[0][1],
-				timestamp:      matched[0][4],
-				method:         matched[0][5],
-				url:            matched[0][6],
-				ProtocolMethod: matched[0][7],
-
-				ServerResponse: matched[0][8],
-				SendBytes:      matched[0][9],
-				userAgent:      matched[0][11],
-				// browser        :,
-				// system         :,
-			}
+func ReadFile(outcome *os.File) []Logs {
+	defer func() {
+		err := outcome.Close()
+		if err != nil {
+			log.Printf("error to open the file %s", err.Error())
+			return
 		}
 	}()
+	rows := []Logs{}
+	scanner := bufio.NewScanner(outcome)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		matched := regularExpression(`^(\S+) (\S+) (\S+) \[([\w:/]+\s[+\-]\d{4})\] "(\S+) (\S+)\s*(\S*)" (\d{3}) (\S+) (\S+) "(.*)"`, scanner.Text())
+		if len(matched) == 0 {
+			continue
+		}
+		rows = append(rows, Logs{
+			ip:             matched[0][1],
+			timestamp:      matched[0][4],
+			method:         matched[0][5],
+			url:            matched[0][6],
+			ProtocolMethod: matched[0][7],
+
+			ServerResponse: matched[0][8],
+			SendBytes:      matched[0][9],
+			userAgent:      matched[0][11],
+			// browser        :,
+			// system         :,
+		})
+	}
 	return rows
 }
 
@@ -107,119 +110,88 @@ func regularExpression(MatchedCase string, resp string) [][]string {
 	return nil
 }
 
-func getIP(logRecord []string) chan []string {
-	ips := make(chan []string)
-	go func() {
-		defer func() {
-			close(ips)
-		}()
-		for _, record := range logRecord {
-			expression := `(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}`
-			matched := regularExpression(expression, record)
-			if matched == nil {
-				continue
-			}
-			for _, sliceOfIPS := range matched {
-				ips <- []string{sliceOfIPS[0]}
-			}
+// uniqueIP eliminates the duplicate data and returns back the unique ips
+func uniqueIP(queue []Logs) []string {
+	keys := make(map[string]bool)
+	list := []string{}
+	log.Println("Processing: Generating unique IP")
+	for _, entry := range queue {
+		if _, value := keys[entry.ip]; !value {
+			keys[entry.ip] = true
+			list = append(list, entry.ip)
 		}
-	}()
-	return ips
-}
-
-// \[\d{1,2}\/\w{3}\/\d{1,4}(:[0-9]{1,2}){3} \+([0-9]){1,4}\]
-
-func getTimeStamp(logRecord []string) chan []string {
-	stamp := make(chan []string)
-	go func() {
-		defer func() {
-			// c, ok := <-stamp
-			// if ok {
-			close(stamp)
-			// }
-			// log.Println("channel still open")
-
-		}()
-		for _, record := range logRecord {
-			expression := `\[\d{1,2}\/\w{3}\/\d{1,4}(:[0-9]{1,2}){3} \+([0-9]){1,4}\]`
-			matched := regularExpression(expression, record)
-			if matched == nil {
-				continue
-			}
-			for _, sliceOfStamps := range matched {
-				stamp <- []string{sliceOfStamps[0]}
-			}
-		}
-	}()
-	return stamp
-}
-
-// TopIPS return the top ips from logs
-func TopIPS(queue map[string]int) {
-	// top := []string{}
-	for i := range queue {
-		// if queue[i] > queue[i]+1 {
-		// 	log.Println(queue[i])
-		// }
-		log.Printf("%d----->%d", queue[i], queue[i])
 	}
+	log.Println("Processing: End")
+	return list
 }
 
-func getUniqueQueue(record []string, length int) map[string]int {
-	if length == 0 || length == 1 {
-		return nil
-	}
+// getUniqueURLQueue checks for the every url and returns back the map of url
+// which holds the count of the each url how many times called
+func getUniqueURLQueue(record []Logs) map[string]int {
 	queue := make(map[string]int)
-	for idx := 0; idx < length-1; idx++ {
-		if record[idx] != record[idx+1] {
-			queue[record[idx]]++
-		}
+	log.Printf("Processing: Generating Unique URL Counts Map")
+	for _, i := range record {
+		queue[i.url]++
 	}
+	log.Println("Processing: End")
 	return queue
 }
 
+func getUniqueMethodQueue(record []Logs) map[string]int {
+	queue := make(map[string]int)
+	log.Printf("Processing: Generating Unique Method Used Counts Map")
+	for _, i := range record {
+		queue[i.method]++
+	}
+	log.Println("Processing: End")
+	return queue
+}
+
+// UpdatedQueue return back the []Logs
+func UpdatedQueue(queue [][]Logs) []Logs {
+	updateQueue := []Logs{}
+	log.Printf("Processing: Updating Queue")
+	for _, updated := range queue {
+		for _, nextQueue := range updated {
+			updateQueue = append(updateQueue, nextQueue)
+		}
+	}
+	log.Println("Processing: End")
+	return updateQueue
+}
+
 func main() {
-	file, err := OpenFile("./access.log")
-	err = ErrorHandling(err, "error to open file", WARNING)
-	if err != nil {
-		log.Println(err)
+	files := flag.String("f", "", "location of the nginx log file comma seperated filenames")
+	flag.Parse()
+	if *files == "" {
+		fmt.Println("Usage: ./logs -f file1,[file2,...]")
+		flag.Usage()
 		return
 	}
-	defer func() {
-		err := file.Close()
+	f := strings.Split(*files, ",")
+	queue := [][]Logs{}
+	for _, path := range f {
+		log.Printf("File Processing Start : [%s]", path)
+		file, err := OpenFile(path)
+		err = ErrorHandling(err, "error to open file", WARNING)
 		if err != nil {
-			log.Printf("error to open the file %s", err.Error())
+			log.Println(err)
 			return
 		}
-	}()
-	rows := ReadFile(file)
-	for i := range rows {
-		log.Println(i)
+		queue = append(queue, ReadFile(file))
+		log.Printf("File Processing End : [%s]", path)
 	}
-	// log.Println(rows)
-	// parallel := 2
-	// var wg sync.WaitGroup
-	// wg.Add(parallel)
-	// go func() {
-	// 	defer wg.Done()
-	// go func() {
-	// ips := getIP(chanToSlice(rows))
-	// converted := chanToSlice(ips)
-	// unique := getUniqueQueue(converted, len(converted))
-	// for key, value := range unique {
-	// 	log.Printf("IP %s >>> %d\n", key, value)
+	updateQueue := UpdatedQueue(queue)
+	// removed the all data from the  [][]Logs
+	log.Println("Processing: Deleting the Old queue so we can use the Memory for other queues")
+	queue = nil
+	log.Println("Processing: End")
+	// CountURLQueue := getUniqueURLQueue(updateQueue)
+	_ = getUniqueURLQueue(updateQueue)
+	// for key, value := range CountURLQueue {
+	// 	log.Printf("URL %s >>> %d\n", key, value)
 	// }
-	// TopIPS(unique)
-	// log.Println(len(unique))
-	// defer close(ips)
-	// }()
-
-	// go func() {
-	// stamp := getTimeStamp(chanToSlice(rows))
-	// // defer close(stamp)
-	// printrecords(stamp)
-	// }()
-	// }()
-	// wg.Wait()
-
+	log.Println(len(uniqueIP(updateQueue)))
+	log.Println(getUniqueMethodQueue(updateQueue))
+	log.Printf("Total Hits >>> %d", len(updateQueue))
 }
