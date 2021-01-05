2ac4a546d918c23cadf21f9efa449e9078bf7128
mavensingh <kuldeep.avsar@gmail.com>
2021-01-05 14:13:42 +0530

    routers

---
+++ routers/route.go
@@ -0,0 +1,19 @@
+package routers
+
+import (
+	"log-parser/controllers"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+//SetupRouter sets up routes
+func SetupRouter() *gin.Engine {
+	router := gin.Default()
+	gin.SetMode(os.Getenv("GIN_MODE"))
+	router.LoadHTMLGlob("./html/templates/*")
+	router.Static("/home", "./html/static")
+	router.GET("/analysis", controllers.MainDashboard)
+	router.GET("/analysis/ip-report", controllers.ReportIP)
+	return router
+}
