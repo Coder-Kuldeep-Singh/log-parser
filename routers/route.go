4a1758b9d4b78ba8462963bc6c31596280c01580
mavensingh <kuldeep.avsar@gmail.com>
2021-01-09 19:43:10 +0530

    router changed

--- routers/route.go
+++ routers/route.go
@@ -13,8 +13,8 @@ func SetupRouter() *gin.Engine {
 	gin.SetMode(os.Getenv("GIN_MODE"))
 	router.LoadHTMLGlob("./html/templates/*")
 	router.Static("/home", "./html/static")
-	router.GET("/analysis", controllers.MainDashboard)
+	router.GET("/", controllers.MainDashboard)
 	router.GET("/analysis/ip-report", controllers.ReportIP)
-	router.POST("api/upload_logs", controllers.GetLogsFromClientSide)
+	router.POST("/api/upload_logs", controllers.GetLogsFromClientSide)
 	return router
 }
