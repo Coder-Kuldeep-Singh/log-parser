ec2a0a5b1ba72486c7ce23f910f6a36cbd437947
mavensingh <kuldeep.avsar@gmail.com>
2021-01-19 19:01:18 +0530

    added new data on the logs dashboard

--- controllers/home.go
+++ controllers/home.go
@@ -1,6 +1,9 @@
 package controllers
 
 import (
+	"log"
+	"log-parser/models"
+	"log-parser/service"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -52,14 +55,29 @@ func MainDashboard(c *gin.Context) {
 	topIps := GetIPs(UpdateQueue)
 	methods := GetMethods(UpdateQueue)
 	referrer := GetReferrer(UpdateQueue)
+	Location := []service.Location{}
+	db, err := OpenLocationDB("./db/GeoLite2-City.mmdb")
+	err = models.ErrorHandling(err, "error to open the location database", models.WARNING)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	for key := range topIps {
+		Location = append(Location, service.GetLocationFromIP(db, key))
+	}
+	db.Close()
 	// log.Println(Nmaximum(code, 3))
+	lastElement := Location[len(Location)-1]
 	c.HTML(http.StatusOK, "dashboard.tmpl.html", gin.H{
-		"TotalHits": len(UpdateQueue),
-		"HTTPCode":  Nmaximum(code, 5),
-		"HTTPError": Nmaximum(errorCode, 3),
-		"TopIPS":    Nmaximum(topIps, 10),
-		"Methods":   Nmaximum(methods, 3),
-		"Referrer":  Nmaximum(referrer, 10),
+		"TotalHits":    len(UpdateQueue),
+		"HTTPCode":     Nmaximum(code, 5),
+		"HTTPError":    Nmaximum(errorCode, 3),
+		"TopIPS":       Nmaximum(topIps, 10),
+		"Methods":      Nmaximum(methods, 3),
+		"Referrer":     Nmaximum(referrer, 10),
+		"Location":     Location[0 : len(Location)-2],
+		"LastLocation": lastElement,
+
 		// "Countries":    GetCountries(Location),
 		// "HTTPCODE":     getHTTPCode(updateQueue),
 		// "HTTPERROR":    getTheErrorStatus(updateQueue),
