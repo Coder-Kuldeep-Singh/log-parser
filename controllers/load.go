f11ee0ea1b5c90f3133e67158a069cdfdd785d06
mavensingh <kuldeep.avsar@gmail.com>
2021-01-21 14:15:31 +0530

    added not found and most visited url counts

--- controllers/load.go
+++ controllers/load.go
@@ -20,7 +20,7 @@ var (
 
 // LoadGlobally loads the data globally
 func LoadGlobally() {
-	f := []string{"./html/static/nginx/access.log", "./html/static/nginx/access.log.1", "./html/static/nginx/access.log.2", "./html/static/nginx/access.log.3", "./html/static/nginx/access.log.4", "./html/static/nginx/access.log.5"}
+	f := []string{"./html/static/nginx/access.log", "./html/static/nginx/access.log.1", "./html/static/nginx/access.log.2", "./html/static/nginx/access.log.3", "./html/static/nginx/access.log.4", "./html/static/nginx/access.log.5", "./html/static/nginx/access.log.6", "./html/static/nginx/access.log.7", "./html/static/nginx/access.log.8", "./html/static/nginx/access.log.9", "./html/static/nginx/access.log.10"}
 	queue := [][]models.Logs{}
 	for _, path := range f {
 		log.Printf("File Processing Start : [%s]", path)
@@ -212,97 +212,39 @@ func UniqueBots(queue []models.Logs) map[string]int {
 	return bots
 }
 
-// // UniqueIP eliminates the duplicate data and returns back the unique ips
-// func UniqueIP() []string {
-// 	keys := make(map[string]bool)
-// 	log.Println("Processing: Generating unique IP")
-// 	for _, entry := range updateQueue {
-// 		if _, value := keys[entry.IP]; !value {
-// 			keys[entry.IP] = true
-// 			list = append(list, entry.IP)
-// 		}
-// 	}
-// 	log.Println("Processing: End")
-// 	return list
-// }
+// NotFoundPages return the all page urls' whose status is not found(404)
+func NotFoundPages(queue []models.Logs) map[string]int {
+	URLQueue := make(map[string]int)
+	for _, newQueue := range queue {
+		if newQueue.URL == "" {
+			continue
+		}
+		if newQueue.ServerResponse == "404" {
+			_, exists := URLQueue[newQueue.URL]
+			if exists {
+				URLQueue[newQueue.URL]++
+			} else {
+				URLQueue[newQueue.URL] = 1
+			}
+		}
 
-// // GetCountries eliminates the duplicate data and returns back the unique ips
-// func GetCountries(queue []service.Location) {
-// 	log.Println("Processing: Generating unique Countries")
-// 	for _, item := range queue {
-// 		_, exist := countries[item.Country]
+	}
+	return URLQueue
+}
 
-// 		if exist {
-// 			countries[item.Country]++ // increase counter by 1 if already in the map
-// 		}
-// 		// else {
-// 		// 	countries[item.Country] = 1 // else start counting from 1
-// 		// }
-// 	}
-// 	log.Println("Processing: End")
-// }
-
-// // GetUniqueURLQueue checks for the every url and returns back the map of url
-// // which holds the count of the each url how many times called
-// func GetUniqueURLQueue() {
-// 	log.Printf("Processing: Generating Unique URL Counts Map")
-// 	for _, item := range updateQueue {
-// 		_, exist := uniqueurlqueue[item.URL]
-// 		if exist {
-// 			uniqueurlqueue[item.URL]++ // increase counter by 1 if already in the map
-// 			// continue
-// 		}
-// 		// uniqueurlqueue[item.URL] = 1 // else start counting from 1
-// 	}
-// 	log.Println("Processing: End")
-// }
-
-// // GetUniqueMethodQueue returns map
-// func GetUniqueMethodQueue() {
-// 	log.Printf("Processing: Generating Unique Method Used Counts Map")
-// 	for _, item := range updateQueue {
-// 		_, exist := methodQueue[item.Method]
-// 		if exist {
-// 			methodQueue[item.Method]++ // increase counter by 1 if already in the map
-// 		}
-// 		// else {
-// 		// 	methodQueue[item.Method] = 1 // else start counting from 1
-// 		// }
-// 	}
-// 	log.Println("Processing: End")
-// }
-
-// func GetHTTPCode() {
-// 	log.Printf("Processing: Generating HTTP Code Count")
-// 	for _, item := range updateQueue {
-// 		_, exist := httpQueue[item.ServerResponse]
-// 		if exist {
-// 			httpQueue[item.ServerResponse]++ // increase counter by 1 if already in the map
-// 		}
-// 		// else {
-// 		// 	httpQueue[item.ServerResponse] = 1 // else start counting from 1
-// 		// }
-// 	}
-// 	log.Println("Processing: End")
-// }
-
-// func GetTheErrorStatus() {
-// 	log.Printf("Processing: Generating HTTP Code Count")
-// 	for _, i := range updateQueue {
-// 		if i.ServerResponse == "" {
-// 			continue
-// 		}
-// 		num, err := strconv.Atoi(i.ServerResponse)
-// 		if err != nil {
-// 			log.Printf("error to convert str to int %s", err.Error())
-// 			continue
-// 		}
-// 		if num >= 299 {
-// 			httpErrorCodeQueue[i.ServerResponse]++
-// 		}
-// 	}
-
-// 	log.Println("Processing: End")
-// }
-
-// func getDuplicate
+// TopVisitedURL return the most visited url counts
+func TopVisitedURL(queue []models.Logs) map[string]int {
+	URLQueue := make(map[string]int)
+	for _, newQueue := range queue {
+		if newQueue.URL == "" {
+			continue
+		}
+		_, exists := URLQueue[newQueue.URL]
+		if exists {
+			URLQueue[newQueue.URL]++
+		} else {
+			URLQueue[newQueue.URL] = 1
+		}
+	}
+	return URLQueue
+}
